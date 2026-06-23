ce1bc78fc188e8b5edf4dd2fec75842f2240a6c6
Emmanuel Fosu <emmanuel.fosuduffour@gmail.com>
2026-06-23 12:38:14 +0000

    Add: base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig, Plugin} from "vite";
 import {resolve } from "node:path";
 
 export default defineConfig({
+    base:'/',
     build: {
         rolldownOptions: {
             input: {
